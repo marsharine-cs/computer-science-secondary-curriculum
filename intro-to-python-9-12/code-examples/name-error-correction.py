4eff4b6a2f9c7a022d62a9fc25d2cb2e67498ed9
Marsharine A. Simpson <103828959+marsharine-cs@users.noreply.github.com>
2026-09-07 09:05:28 -0400

    Add verified Python debugging examples

---
+++ intro-to-python-9-12/code-examples/name-error-correction.py
@@ -0,0 +1,12 @@
+"""Public portfolio example for Lesson 1.3: Reading Errors as Information.
+
+Verified with Python 3.12.
+"""
+
+# Broken version from the lesson:
+# print(student_name)
+# NameError: name 'student_name' is not defined
+
+# Corrected version:
+student_name = "Jordan"
+print(student_name)
